0164085dfe1da36f679b90a2abaa975809df2b28
ryanwindham3 <90585845+ryanwindham3@users.noreply.github.com>
2021-10-23 12:41:44 -0500

    Initial commit

---
+++ forms/forReverseStates/forReverseStates.js
@@ -0,0 +1,6 @@
+
+/*let states = ["Nebraska", "Iowa", "Minnesota", "South Dakota", "North Dakota", "Kansas", "Oklahoma", "Texas"]
+
+for (i = states.length - 1; i >= 0; i--)
+  console.log(states[i])
+  */
